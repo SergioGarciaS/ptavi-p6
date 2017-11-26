017fb07b6a7f5aaeb678597fc35413c63bcedcd7
Garcias <sergio.garcia.sti@gmail.com>
2017-11-26 14:26:03 +0100

    realizada la secuencia de envio archivo rtp

--- client.py
+++ client.py
@@ -5,17 +5,19 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
-import sys 
+import sys
 
 if len(sys.argv) == 3:
     Usuario = sys.argv[2].split(':')
-    PORT = Usuario[1]
+    PORT = int(Usuario[1])
     USER = Usuario[0]
-    Methods = [invite, bye]
-    def MakeUSERM(protocol)
-    """ función para crear la cabecera del datagrama """
-        if sys.argv[1] in Methods:
-            USER_M = str(sys.argv[1].upper +' sip:' + USER)
+    SERVER = USER.split('@')[1]
+    Methods = ['invite', 'bye']
+    USER_M = '';
+    print(sys.argv[1])
+    print(Usuario, PORT, USER, SERVER);
+    if sys.argv[1] in Methods:
+        USER_M = sys.argv[1].upper() + ' sip:' + USER
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -25,10 +27,14 @@ if len(sys.argv) == 3:
         my_socket.send(bytes(Data, 'utf-8'))
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
-        
-        if noseque == 200 ok:
-            mandamos el mensaje de ok.
-
+        respuesta = data.decode('utf-8').split('\r\n\r\n')[0:-1]
+        if sys.argv[1] == 'invite' and respuesta == ['SIP/2.0 100 Trying',
+                                                    'SIP/2.0 180 Ringing',
+                                                    'SIP/2.0 200 OK']:
+            USER_M = 'ACK' + ' sip:' + USER
+            Data = USER_M + ' ' + 'SIP/2.0\r\n\r\n'
+            print("Enviando:", USER_M)
+            my_socket.send(bytes(Data, 'utf-8'))
     print("Socket terminado.")
 else:
     sys.exit('Usage: client.py method receiver@ip:SIPport')
