190cb7a6be93f16e04f29878439a7f661fa880a4
Sergio Garcia Sanchez <garcias@gamma13.aulas.gsyc.urjc.es>
2017-11-27 15:22:31 +0100

    Pasado pep8

--- client.py
+++ client.py
@@ -13,9 +13,7 @@ if len(sys.argv) == 3:
     USER = Usuario[0]
     SERVER = USER.split('@')[1]
     Methods = ['invite', 'bye']
-    USER_M = '';
-    print(sys.argv[1])
-    print(Usuario, PORT, USER, SERVER);
+    USER_M = ''
     if sys.argv[1] in Methods:
         USER_M = sys.argv[1].upper() + ' sip:' + USER
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
@@ -29,8 +27,8 @@ if len(sys.argv) == 3:
         print('Recibido -- ', data.decode('utf-8'))
         respuesta = data.decode('utf-8').split('\r\n\r\n')[0:-1]
         if sys.argv[1] == 'invite' and respuesta == ['SIP/2.0 100 Trying',
-                                                    'SIP/2.0 180 Ringing',
-                                                    'SIP/2.0 200 OK']:
+                                                     'SIP/2.0 180 Ringing',
+                                                     'SIP/2.0 200 OK']:
             USER_M = 'ACK' + ' sip:' + USER
             Data = USER_M + ' ' + 'SIP/2.0\r\n\r\n'
             print("Enviando:", USER_M)
@@ -38,4 +36,3 @@ if len(sys.argv) == 3:
     print("Socket terminado.")
 else:
     sys.exit('Usage: client.py method receiver@ip:SIPport')
-
