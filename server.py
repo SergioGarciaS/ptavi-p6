a1e661a9f980c946235b69131b9e1bb7ef3511e9
Sergio Garcia Sanchez <garcias@gamma16.aulas.gsyc.urjc.es>
2017-11-27 10:12:54 +0100

    uso sin errores correcto

--- server.py
+++ server.py
@@ -14,25 +14,18 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
 
-
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        #self.wfile.write(b"Hemos recibido tu peticion")
-        if len(sys.argv) == 4:
-            IP = sys.argv[1]
-            PORT = sys.argv[2]
-            FILE = sys.argv[3]
-            print(IP, PORT, FILE)
-        else:
-            sys.exit('Usage: server.py IP PORT AUDIOFILE')
-
 
         Methods = ['INVITE', 'BYE','ACK']
         IP_Client = str(self.client_address[0])
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print(line.decode('utf-8').split(' ')[0])
+            if not line:
+                break
+            
+            print("El cliente nos manda " + line.decode('utf-8'))
             if line.decode('utf-8').split(' ')[0] == 'INVITE':
                 Answer = ('SIP/2.0 100 Trying' + '\r\n\r\n' +
                           'SIP/2.0 180 Ringing' + '\r\n\r\n' +
@@ -45,17 +38,28 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 toRun = ('mp32rtp -i ' + IP_Client + ' -p 23032 < ' + FILE)
                 print("Vamos a ejecutar", toRun)
                 os.system(toRun)
-                print("finish")
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
-
-            print("El cliente nos manda " + line.decode('utf-8'))
+                print("*===================*\n",
+                      "The file is send...\n",
+                      "*===================*\n")
+                
+            elif line.decode('utf-8').split(' ')[0] not in Methods:
+                Answer = ('SIP/2.0 400 Bad Request' + '\r\n\r\n')
+                self.wfile.write(bytes(Answer, 'utf-8'))
+            else:
+                Answer = ('SIP/2.0 405 Method Not allowed' + '\r\n\r\n')
+                self.wfile.write(bytes(Answer, 'utf-8'))
+            
 
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    if len(sys.argv) == 4:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        FILE = sys.argv[3]
+    else:
+        sys.exit('Usage: server.py IP PORT AUDIOFILE')
+
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    print("Listening...")
     serv.serve_forever()
